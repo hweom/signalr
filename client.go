1fd4c4ddf88e5a050b4a9946ef1e9ec11f9b4270
Mikhail Balakhno <mikhail.balakhno@gmail.com>
2017-08-12 22:16:49 -0700

    Changed control flow model

--- client.go
+++ client.go
@@ -2,9 +2,11 @@ package signalr
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/url"
+	"sync"
 
 	"github.com/gorilla/websocket"
 )
@@ -23,9 +25,23 @@ type negotiationResponse struct {
 }
 
 type Client struct {
-	params negotiationResponse
-	socket *websocket.Conn
-	nextId int
+	OnMessageError func(err error)
+	OnClientMethod func(hub, method string, arguments []json.RawMessage)
+	params         negotiationResponse
+	socket         *websocket.Conn
+	nextId         int
+
+	// Futures for server call responses and a guarding mutex.
+	responseFutures map[string]chan *serverMessage
+	mutex           sync.Mutex
+}
+
+type serverMessage struct {
+	Cursor     string            `json:"C"`
+	Data       []json.RawMessage `json:"M"`
+	Result     json.RawMessage   `json:"R"`
+	Identifier string            `json:"I"`
+	Error      string            `json:"E"`
 }
 
 func negotiate(scheme, address string) (negotiationResponse, error) {
@@ -79,12 +95,66 @@ func connectWebsocket(address string, params negotiationResponse, hubs []string)
 	}
 }
 
-func (self *Client) Read() ([]byte, error) {
-	_, data, err := self.socket.ReadMessage()
-	return data, err
+func (self *Client) routeResponse(response *serverMessage) {
+	self.mutex.Lock()
+	defer self.mutex.Unlock()
+
+	if c, ok := self.responseFutures[response.Identifier]; ok {
+		c <- response
+		close(c)
+		delete(self.responseFutures, response.Identifier)
+	}
 }
 
-func (self *Client) CallHub(hub, method string, params ...interface{}) error {
+func (self *Client) createResponseFuture(identifier string) chan *serverMessage {
+	var c = make(chan *serverMessage)
+
+	self.mutex.Lock()
+	defer self.mutex.Unlock()
+
+	self.responseFutures[identifier] = c
+
+	return c
+}
+
+// Start dispatch loop. This function will return when error occurs. When this
+// happens, all the connections are closed and user can run Connect()
+// and Dispatch() again on the same client.
+func (self *Client) Dispatch() error {
+	for {
+		var message serverMessage
+
+		var hubCall struct {
+			HubName   string            `json:"H"`
+			Method    string            `json:"M"`
+			Arguments []json.RawMessage `json:"A"`
+		}
+
+		_, data, err := self.socket.ReadMessage()
+		if err != nil {
+			self.socket.Close()
+			return err
+		} else if err := json.Unmarshal(data, &message); err != nil {
+			if self.OnMessageError != nil {
+				self.OnMessageError(err)
+			}
+		} else {
+			if len(message.Identifier) > 0 {
+				// This is a response to a hub call.
+				self.routeResponse(&message)
+			} else if len(message.Data) == 1 {
+				if err := json.Unmarshal(message.Data[0], &hubCall); err == nil && len(hubCall.HubName) > 0 && len(hubCall.Method) > 0 {
+					// This is a client Hub method call from server.
+					if self.OnClientMethod != nil {
+						self.OnClientMethod(hubCall.HubName, hubCall.Method, hubCall.Arguments)
+					}
+				}
+			}
+		}
+	}
+}
+
+func (self *Client) CallHub(hub, method string, params ...interface{}) ([]json.RawMessage, error) {
 	var request = struct {
 		Hub        string        `json:"H"`
 		Method     string        `json:"M"`
@@ -101,30 +171,45 @@ func (self *Client) CallHub(hub, method string, params ...interface{}) error {
 
 	data, err := json.Marshal(request)
 	if err != nil {
+		return nil, err
+	}
+
+	var responseChannel = self.createResponseFuture(fmt.Sprintf("%d", request.Identifier))
+
+	if err := self.socket.WriteMessage(websocket.TextMessage, data); err != nil {
+		return nil, err
+	}
+
+	if response, ok := <-responseChannel; !ok {
+		return nil, fmt.Errorf("Call to server returned no result")
+	} else if len(response.Error) > 0 {
+		return nil, fmt.Errorf("%s", response.Error)
+	} else {
+		return response.Data, nil
+	}
+}
+
+func (self *Client) Connect(scheme, host string, hubs []string) error {
+	// Negotiate parameters.
+	if params, err := negotiate(scheme, host); err != nil {
 		return err
-	} else if err := self.socket.WriteMessage(websocket.TextMessage, data); err != nil {
+	} else {
+		self.params = params
+	}
+
+	// Connect Websocket.
+	if ws, err := connectWebsocket(host, self.params, hubs); err != nil {
 		return err
+	} else {
+		self.socket = ws
 	}
 
 	return nil
 }
 
-func NewWebsocketClient(scheme, host string, hubs []string) (*Client, error) {
-	// Negotiate parameters.
-	params, err := negotiate(scheme, host)
-	if err != nil {
-		return nil, err
-	}
-
-	// Connect Websocket.
-	ws, err := connectWebsocket(host, params, hubs)
-	if err != nil {
-		return nil, err
-	}
-
+func NewWebsocketClient() *Client {
 	return &Client{
-		params: params,
-		socket: ws,
-		nextId: 1,
-	}, nil
+		nextId:          1,
+		responseFutures: make(map[string]chan *serverMessage),
+	}
 }
